2c5f5ccf7f76a8fcec424b1f471d849d456254a8
Ilham Imani Nur Alam <ilhamimani@Ilhams-MacBook-Pro.local>
2026-02-10 21:05:34 +0700

    Day 3 - Struct and TTD

---
+++ shapes/shapes.go
@@ -0,0 +1,36 @@
+package shapes
+
+import "math"
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+type Circle struct {
+	Radius float64
+}
+
+type Shape interface {
+	Area() float64
+}
+
+type Triangle struct {
+	Base   float64
+	Height float64
+}
+
+func Perimeter(rectangle Rectangle) float64 {
+	return 2 * (rectangle.Height + rectangle.Width)
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Width * r.Height
+}
+
+func (circle Circle) Area() float64 {
+	return math.Pi * circle.Radius * circle.Radius
+}
+
+func (t Triangle) Area() float64 {
+	return (t.Base * t.Height) / 2
+}
